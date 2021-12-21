b0e9b142faf7b9f8d509f752a2115ae6990c3f92
sejun3278 <sejun3278@gmail.com>
2021-12-21 12:00:29 +0900

    2021-12-21-modify-imgae

--- pages/boards/index.js
+++ pages/boards/index.js
@@ -5,7 +5,7 @@ export default function BoardsPage() {
         <>
             <Head>
                 <meta property="og:title" content="보드 페이지 입니다." />
-                <meta property="og:image" content="https://codebootcamp.co.kr/images/main/main_tag_1.webp" />
+                <meta property="og:image" content="https://www.itlab.co.kr/v6/_sys/_upload/image/200903/30/123839555983.jpg" />
                 <meta property="og:description" content="가나다라마바사" />
                 <meta property="og:url" content="https://sjcode4.tk/" />
             </Head>
