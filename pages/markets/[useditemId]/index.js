1637cdb9ad0c2f1cbc522d47f7e8ab0e2881585e
sejun3278 <sejun3278@gmail.com>
2021-12-21 13:03:45 +0900

    2021-12-21-ssr-complate

---
+++ pages/markets/[useditemId]/index.js
@@ -0,0 +1,43 @@
+import Head from "next/head"
+import { gql, request } from "graphql-request"
+
+export default function MarketPage(props) {
+    console.log(props)
+    return(
+        <>
+            <Head>
+                <meta property="og:title" content={props.result.fetchUseditem.name}/>
+                <meta property="og:image" content={props.result.fetchUseditem.images[0]} />
+                <meta property="og:description" content={props.result.fetchUseditem.remarks} />
+                <meta property="og:url" content="https://sjcode4.tk/" />
+            </Head>
+            <div>
+                마켓 페이지 입니다.
+            </div>
+        </>
+    )
+}
+
+const FETCH_USEDITEM = gql`
+    query fetchUseditem($useditemId : ID!) {
+        fetchUseditem(useditemId : $useditemID) {
+            images
+            name
+            remarks
+        }
+    }
+`
+
+export const getServerSideProps = async (context) => {
+    const result = await request(
+        "https://backend04.codebootcamp.co.kr/graphql", 
+        FETCH_USEDITEM, 
+        { useditemId : context.query.useditemId }
+    )
+
+    return {
+        props : {
+            result
+        }
+    }
+}
