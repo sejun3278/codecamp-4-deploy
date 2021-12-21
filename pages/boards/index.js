60385749f49156b8c57ac80ac67fd6f019fb5242
sejun3278 <sejun3278@gmail.com>
2021-12-21 11:24:50 +0900

    2021-12-21-modify-meta

---
+++ pages/boards/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+export default function BoardsPage() {
+    return(
+        <>
+            <Head>
+                <meta property="og:title" content="보드 페이지 입니다." />
+                <meta property="og:image" content="https://codebootcamp.co.kr/images/main/main_tag_1.webp" />
+                <meta property="og:description" content="가나다라마바사" />
+                <meta property="og:url" content="https://sjcode4.tk/" />
+            </Head>
+            <div>
+                보드 페이지 입니다.
+            </div>
+        </>
+    )
+}
